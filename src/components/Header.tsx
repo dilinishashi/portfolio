43f89e47653d776886767ddc33a76814412cd40f
Inamul Hasan <inamul.h@medoment.com>
2025-09-02 09:38:47 +0530

    changed the name for the top tab

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
       <div className="container flex h-14 items-center">
         <div className="mr-4 hidden md:flex">
           <a href="/" className="mr-6 flex items-center space-x-2">
-            <span className="hidden font-bold sm:inline-block">Hasan Bose</span>
+            <span className="hidden font-bold sm:inline-block">InamulHasan</span>
           </a>
           <nav className="flex items-center space-x-6 text-sm font-medium">
             {navLinks.map((link) => (
